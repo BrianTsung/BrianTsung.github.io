15aeed0c9b7d73a5aa5d34b3f21dda65d3802f27
DESKTOP-1VROJ6F\brian <brian861024@gmail.com>
2021-03-24 16:59:13 +0800

    完成基礎功能,use webpack

--- MOTC_Transport/App.js
+++ MOTC_Transport/App.js
@@ -1,47 +1,105 @@
 import axios from "axios";
+import React from "react";
+import ReactDOM from "react-dom";
 
-function HandleSubmit(event) {
-  event.preventDefault();
-  axios
-    .get(
-      `https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$top=30&$format=JSON`
-    )
-    .then((response) => {
-      console.log(response);
-      // this.setState({ information: response.data });
-      // console.log(this.state.information);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
+const API_URL = "https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot";
+
+function Attractions(props) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th className="table_name">景點名稱</th>
+          <th className="table_des">景點描述</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.datas.map((data) => (
+          <tr key={data.ID}>
+            <td>{data.Name}</td>
+            <td>{data.Description}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.selectall = this.selectall.bind(this);
-    this.state = { isall: true };
+    this.isBottom = this.isBottom.bind(this);
+    this.state = { isall: true, datas: [], top: 30 };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    const url = `${API_URL}?$top=${this.state.top}&$format=JSON`;
+    axios
+      .get(url)
+      .then((response) => response.data)
+      .then((data) => {
+        this.setState({ datas: data });
+      });
+    document.addEventListener("scroll", this.isBottom);
+  }
 
-  componentWillUnmount() {}
+  componentWillUnmount() {
+    document.removeEventListener("scroll", this.isBottom);
+  }
 
-  selectall(event) {
-    this.setState({ isall: !this.state.isall });
+  selectall() {
+    this.setState({ isall: !this.state.isall, top: 30 }); // bug
+    let url;
+
+    if (!this.state.isall) {
+      url = `${API_URL}?$top=${this.state.top}&$format=JSON`;
+    } else {
+      url = `${API_URL}/Taipei?$top=${this.state.top}&$format=JSON`;
+    }
+    axios
+      .get(url)
+      .then((response) => response.data)
+      .then((data) => {
+        this.setState({ datas: data });
+      });
+  }
+
+  isBottom() {
+    const wrappedElement = document.getElementById("root");
+    let bottom = wrappedElement.getBoundingClientRect().bottom;
+    let window_height = window.innerHeight;
+    let url;
+
+    if (Math.floor(bottom) <= window_height) {
+      this.setState({ top: this.state.top + 30 });
+      if (this.state.isall) {
+        url = `${API_URL}?$top=${this.state.top}&$format=JSON`;
+      } else {
+        url = `${API_URL}/Taipei?$top=${this.state.top}&$format=JSON`;
+      }
+      axios
+        .get(url)
+        .then((response) => response.data)
+        .then((data) => {
+          this.setState({ datas: data });
+        });
+    }
   }
 
   render() {
     const isall = this.state.isall;
+    console.log("render", isall);
+    console.log("top", this.state.top);
     let head;
     if (isall) {
       head = (
         <div>
-          <button className="headerbtn active">全部景點<HandleSubmit></HandleSubmit></button>
+          <button className="headerbtn active">全部景點</button>
           <button className="headerbtn nonactive" onClick={this.selectall}>
             縣市景點
           </button>
-          全部
+          <Attractions datas={this.state.datas}></Attractions>
         </div>
       );
     } else {
@@ -51,7 +109,7 @@ class App extends React.Component {
             全部景點
           </button>
           <button className="headerbtn active">縣市景點</button>
-          縣市
+          <Attractions datas={this.state.datas}></Attractions>
         </div>
       );
     }
