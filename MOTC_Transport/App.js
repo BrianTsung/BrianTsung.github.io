a96d9e84055f62a8fa180ecb7b57c3798ac83a79
DESKTOP-1VROJ6F\brian <brian861024@gmail.com>
2021-03-31 09:19:07 +0800

    增加選擇縣市景點功能，加入boostrap，修正載入bug

--- MOTC_Transport/App.js
+++ MOTC_Transport/App.js
@@ -6,7 +6,7 @@ const API_URL = "https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot";
 
 function Attractions(props) {
   return (
-    <table>
+    <table className="table table-bordered table-hover">
       <thead>
         <tr>
           <th className="table_name">景點名稱</th>
@@ -25,12 +25,25 @@ function Attractions(props) {
   );
 }
 
+function Selectcity(props) {
+  <form>
+    <select value={this.state.city} onChange={this.handleChange}>
+      <option value="Taipei">台北市</option>
+      <option value="NewTaipei">新北市</option>
+      <option value="Taoyuan">桃園市</option>
+      <option value="Taichung">台中市</option>
+    </select>
+    <br />
+  </form>;
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.selectall = this.selectall.bind(this);
     this.isBottom = this.isBottom.bind(this);
-    this.state = { isall: true, datas: [], top: 30 };
+    this.selectcity = this.selectcity.bind(this);
+    this.state = { isall: true, datas: [], top: 30, city: "Taipei" };
   }
 
   componentDidMount() {
@@ -53,9 +66,9 @@ class App extends React.Component {
     let url;
 
     if (!this.state.isall) {
-      url = `${API_URL}?$top=${this.state.top}&$format=JSON`;
+      url = `${API_URL}?$top=30&$format=JSON`;
     } else {
-      url = `${API_URL}/Taipei?$top=${this.state.top}&$format=JSON`;
+      url = `${API_URL}/Taipei?$top=30&$format=JSON`;
     }
     axios
       .get(url)
@@ -76,7 +89,7 @@ class App extends React.Component {
       if (this.state.isall) {
         url = `${API_URL}?$top=${this.state.top}&$format=JSON`;
       } else {
-        url = `${API_URL}/Taipei?$top=${this.state.top}&$format=JSON`;
+        url = `${API_URL}/${this.state.city}?$top=${this.state.top}&$format=JSON`;
       }
       axios
         .get(url)
@@ -85,6 +98,18 @@ class App extends React.Component {
           this.setState({ datas: data });
         });
     }
+  }
+
+  selectcity(event) {
+    this.setState({ city: event.target.value, top: 30 });
+    let url;
+    url = `${API_URL}/${event.target.value}?$top=30&$format=JSON`;
+    axios
+      .get(url)
+      .then((response) => response.data)
+      .then((data) => {
+        this.setState({ datas: data });
+      });
   }
 
   render() {
@@ -99,21 +124,54 @@ class App extends React.Component {
           <button className="headerbtn nonactive" onClick={this.selectall}>
             縣市景點
           </button>
-          <Attractions datas={this.state.datas}></Attractions>
         </div>
       );
     } else {
       head = (
         <div>
-          <button className="headerbtn nonactive" onClick={this.selectall}>
-            全部景點
-          </button>
-          <button className="headerbtn active">縣市景點</button>
-          <Attractions datas={this.state.datas}></Attractions>
+          <div>
+            <button className="headerbtn nonactive" onClick={this.selectall}>
+              全部景點
+            </button>
+            <button className="headerbtn active">縣市景點</button>
+          </div>　 
+          <div>
+            <form>
+              <select className="form-select" value={this.state.city} onChange={this.selectcity}>
+                <option value="Taipei">台北市</option>
+                <option value="NewTaipei">新北市</option>
+                <option value="Taoyuan">桃園市</option>
+                <option value="Taichung">台中市</option>
+                <option value="Tainan">臺南市</option>
+                <option value="Kaohsiung">高雄市</option>
+                <option value="Keelung">基隆市</option>
+                <option value="Hsinchu">新竹市</option>
+                <option value="HsinchuCounty">新竹縣</option>
+                <option value="MiaoliCounty">苗栗縣</option>
+                <option value="NantouCounty">南投縣</option>
+                <option value="NantouCounty">彰化縣</option>
+                <option value="YunlinCounty">雲林縣</option>
+                <option value="ChiayiCounty">嘉義縣</option>
+                <option value="Chiayi">嘉義市</option>
+                <option value="PingtungCounty">屏東縣</option>
+                <option value="YilanCounty">宜蘭縣</option>
+                <option value="HualienCounty">花蓮縣</option>
+                <option value="TaitungCounty">臺東縣</option>
+                <option value="KinmenCounty">金門縣</option>
+                <option value="PenghuCounty">澎湖縣</option>
+                <option value="LienchiangCounty">連江縣</option>
+              </select>
+            </form>
+          </div>
         </div>
       );
     }
-    return head;
+    return (
+      <React.Fragment>
+        {head}
+        <Attractions datas={this.state.datas}></Attractions>
+      </React.Fragment>
+    );
   }
 }
 
